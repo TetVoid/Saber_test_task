ca2d5a65bcc9feb560cd6ddec50589638cc34ace
TetVoid <nikbot58@gmail.com>
2021-09-28 00:35:52 +0300

    saber second task: status done

--- saber_test_task/saber_test_task.cpp
+++ saber_test_task/saber_test_task.cpp
@@ -9,10 +9,10 @@ int* intToByte(int number)
         number *= -1;
         negativeFlag = true;
     }
+
     number++;
     int countOfBit = ceil(log2(number))+1;
     number--;
-
 
     int* byteNumber = new int[countOfBit];
 
@@ -20,6 +20,8 @@ int* intToByte(int number)
         byteNumber[0] = 1;
     else
         byteNumber[0] = 0;
+
+
 
     for (int i = countOfBit-1; i > 0; i--)
     {
@@ -30,10 +32,45 @@ int* intToByte(int number)
     for (int i = 0; i < countOfBit; i++)
         std::cout << byteNumber[i];
 
+    std::cout << std::endl;
     return  byteNumber;
+}
+
+void RemoveDups(char* str)
+{
+    int index = 0;
+    int skip_symbols = 0;
+    while (str[index] != '\0')
+    {
+        if (str[index] == str[index + 1])
+            skip_symbols++;
+        else
+        {
+            if(skip_symbols !=0 )
+            {
+                int index_copy = index;
+                while (str[index_copy] != '\0')
+                {
+                    str[index_copy - skip_symbols] = str[index_copy];
+                    index_copy++;
+                }
+                str[index_copy-skip_symbols] = '\0';
+            }
+            index -= skip_symbols;
+            skip_symbols = 0;
+        }
+
+        index++;
+    }
 }
 
 int main()
 {
-    intToByte(999);
+    intToByte(-16);
+
+    char data[] = "aaaaaaa ggggggjjjjgg hhh";
+    RemoveDups(data);
+    std::cout <<data;
+
+
 }
